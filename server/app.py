df30415088ad2528894dc974c56d8b8aa0b8124e
perchik_air <1919ars@gmail.com>
2022-06-03 01:21:34 +0300

    more

--- server/app.py
+++ server/app.py
@@ -49,16 +49,16 @@ stocks_names_list = [
     'VOO',
 ]
 
-app = Flask(__name__, static_url_path='/', static_folder='../client/build')
+app = Flask(__name__, static_url_path='', static_folder='../client/build')
 app.secret_key = os.environ.get('SECRET_KEY')
 if mode == 'build':
     print(mode)
     CORS(app)  # comment this on deployment
 
 
-@app.route('/')
-def index_func():
-    return send_from_directory('../client/build', 'index.html')
+@app.route('/', defaults={'path': ''})
+def index_func(path):
+    return send_from_directory(app.static_folder, 'index.html')
     # return 'hello'
 
 
